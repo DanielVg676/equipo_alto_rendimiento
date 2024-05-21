5cf9b15409068680bde6fee081ed512253f8807b
DanielVg676 <156235653+DanielVg676@users.noreply.github.com>
2024-05-21 07:51:02 -0600

    .

--- index.py
+++ index.py
@@ -1,1 +1,37 @@
+import turtle
+
+def cuadrado_simple():
+    obj = turtle.Turtle()
+
+    for i in range(4):
+        obj.forward(100)
+        obj.right(90)
+
+    turtle.done()
+
+
+def tablero():
+    ventana = turtle.Screen()
+    tortuga = turtle.Turtle()
+    tortuga.forward(300)
+    tortuga.left(90)
+    tortuga.forward(300)
+    tortuga.left(90)
+    tortuga.forward(300)
+    tortuga.left(90)
+    tortuga.forward(300)
+    tortuga.left(90)
+    penup()
+    tortuga.forward(100)
+    tortuga.left(90)
+    pendown()
+    tortuga.forward(100)
+    
+
+
+    ventana.mainloop()
+
+
+tablero()
+
 
